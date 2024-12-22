6ea5bb661f322cbf6259d9f8f679dcf6940b0992
xxhls <xxhls@vip.qq.com>
2024-12-22 15:34:24 +0800

    feat: 获取微信数量

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut stored_value: i32 = 0;
+
+    loop {
+        println!("请输入一个待开启的微信数量 (输入 'q' 退出):");
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("读取输入失败");
+
+        let input = input.trim();
+        if input == "q" {
+            println!("程序退出");
+            break;
+        }
+
+        match input.parse::<i32>() {
+            Ok(num) => {
+                stored_value = num;
+                break;
+            }
+            Err(_) => {
+                println!("输入无效，请输入一个合法的整数");
+            }
+        }
+    }
+
+    if stored_value <= 0 {
+        println!("输入的值必须大于 0");
+        return;
+    }
+
+    println!("开始开启 {} 个微信", stored_value);
 }
